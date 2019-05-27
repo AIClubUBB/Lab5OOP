587297dc6101b747c5aff43f9a0055f813f9ee1c
AIClubUBB <49077580+AIClubUBB@users.noreply.github.com>
2019-05-27 17:02:20 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,13 +1,11 @@
-#include "UI.h"
-#include "Tests.h"
+#include "mainwindow.h"
+#include <QApplication>
 
-int main() {
-    UI ui=UI();
-    ui.run();
-    //testAll();
-    /*string baseUrl;
-    baseUrl="https://www.google.com/";
-    string completeUrl=baseUrl;
-    system(std::string("start " + completeUrl).c_str());*/
-	return 0;
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
 }
